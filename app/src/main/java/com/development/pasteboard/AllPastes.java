4efaf24c411ec7c1e9379e7b5bc3fc3f909ae131
chinmaester <yushendye@gmial.com>
2020-09-18 17:06:48 +0530

    Commited on Friday 18 Sept 2020

---
+++ app/src/main/java/com/development/pasteboard/AllPastes.java
@@ -0,0 +1,14 @@
+package com.development.pasteboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllPastes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_pastes);
+    }
+}
---
+++ app/src/main/java/com/development/pasteboard/MainActivity.java
@@ -0,0 +1,20 @@
+package com.development.pasteboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go_to_register(View view){
+        Intent i = new Intent(MainActivity.this, UserRegistration.class);
+        startActivity(i);
+    }
+}
